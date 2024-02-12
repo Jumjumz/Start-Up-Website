0393e2ab17bbae6ac3764b72dec0959810d4ba05
Jumjum Ramos <jumjumramos@gmail.com>
2024-02-12 22:38:46 +0800

    add blank target in github link

--- src/components/Codelink/codelink.tsx
+++ src/components/Codelink/codelink.tsx
@@ -5,7 +5,9 @@ function codelink() {
     <section className="git-sec">
       <div className="git-cont">
         <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" />
-        <a href="https://github.com/Jumjumz/Start-Up-Website">Code Here</a>
+        <a href="https://github.com/Jumjumz/Start-Up-Website" target="_blank">
+          Code Here
+        </a>
       </div>
     </section>
   );
